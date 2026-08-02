b2b6ee08fb049f80846036793dcc45f0523e9480
Puleeno Nguyen <puleeno@gmail.com>
2026-08-02 22:34:11 +0700

    chore: add Composer autoloader files to custom login page extension

---
+++ libs/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'jankx/custom-login-page',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '40e1ada4d28443412a9a565d3d79e251e956163e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'jankx/custom-login-page' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '40e1ada4d28443412a9a565d3d79e251e956163e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
