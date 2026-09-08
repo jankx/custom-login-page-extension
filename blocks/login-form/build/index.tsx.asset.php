831562f94d01d35a213b31c575822642e98d004a
Puleeno Nguyen <puleeno@gmail.com>
2026-09-09 06:53:09 +0700

    refactor: rebuild custom login block assets and add missing style source maps

--- blocks/login-form/build/index.tsx.asset.php
+++ blocks/login-form/build/index.tsx.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '5874b8ab04e5eca9cc46');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '5bcfe88d02f4743679d2');
--- blocks/login-page/build/index.tsx.asset.php
+++ blocks/login-page/build/index.tsx.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'bb9afbc9b73d7bb270a4');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'a57326602ce44238834b');
