613881843eec83504e89914caaea293c19a6fc70
Puleeno Nguyen <puleeno@gmail.com>
2026-09-19 03:26:41 +0700

    Remove unused styles and scripts for login and register forms; clean up build artifacts

--- blocks/login-page/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '5f9fbb7d88cd68b16416');
---
+++ blocks/login-page/build/index.tsx.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '5cb8846a71f2da87ac5e');
